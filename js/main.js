5436a3dad98995420ffb82ed51747d46cc277697
namnhi997 <namnhi.vnu@gmail.com>
2022-03-30 14:20:23 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,75 @@
-const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+// const swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
 
-    grid: {
-        rows: 2,
-    },
-    spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 3000,
-    },
-});
-const bemac = new Swiper(".bemac", {
-    slidesPerView: 3,
+//     grid: {
+//         rows: 2,
+//     },
+//     spaceBetween: 10,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     autoplay: {
+//         delay: 3000,
+//     },
+// });
+// const bemac = new Swiper(".bemac", {
+//     slidesPerView: 3,
 
-    grid: {
-        rows: 2,
-    },
-    spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 4000,
-    },
-});
+//     grid: {
+//         rows: 2,
+//     },
+//     spaceBetween: 10,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     autoplay: {
+//         delay: 4000,
+//     },
+// });
+// const bechoi = new Swiper(".bechoi", {
+//     slidesPerView: 3,
+
+//     grid: {
+//         rows: 2,
+//     },
+//     spaceBetween: 10,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     autoplay: {
+//         delay: 3000,
+//     },
+// });
+// const bekhoe = new Swiper(".bekhoe", {
+//     slidesPerView: 3,
+
+//     grid: {
+//         rows: 2,
+//     },
+//     spaceBetween: 10,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     autoplay: {
+//         delay: 4000,
+//     },
+// });
+// const dogiadung = new Swiper(".dogiadung", {
+//     slidesPerView: 3,
+
+//     grid: {
+//         rows: 2,
+//     },
+//     spaceBetween: 10,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     autoplay: {
+//         delay: 4000,
+//     },
+// });
